4547e31ac2a5d5d68575a448d9d390d363fa5157
user <user@PC>
2023-03-05 13:53:20 -0500

    Add project files.

---
+++ bookStoreRecord/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace recordscratch
+{
+    public record Bookstore(int ID, string Title, string Author, double Price);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bookstore bookstore1 = new Bookstore(1, "title1", "author1", 1.50);
+            Bookstore bookstore2 = new Bookstore(2, "title2", "author2", 2.50);
+            Bookstore bookstore3 = new Bookstore(3, "title3", "author3", 3.50);
+            Console.WriteLine(bookstore1);
+            Console.WriteLine("///");
+            Console.WriteLine(bookstore2);
+            Console.WriteLine("///");
+            Console.WriteLine(bookstore3);
+        }
+    }
+}
